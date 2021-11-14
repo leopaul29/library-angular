2eb99e82bab26ae9f1896e9eebeb33ef79606c00
Leo-paul <leopaulmartin@hotmail.fr>
2021-11-14 19:32:05 +0100

    add home, list and form components and routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BookListComponent } from './book-list/book-list.component';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { BookFormComponent } from './book-form/book-form.component';
 
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'list', component: BookListComponent },
+  { path: 'create', component: BookFormComponent }, 
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookListComponent,
+    HomeComponent,
+    BookFormComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
