c3cf95bf2d95b08fa9b7d10b612b6f95e37ca9cd
Leo-paul <leopaulmartin@hotmail.fr>
2021-11-18 23:57:03 +0100

    remove books mock data

--- src/app/book.service.ts
+++ src/app/book.service.ts
@@ -8,31 +8,12 @@ import { environment } from 'src/environments/environment';
   providedIn: 'root',
 })
 export class BookService {
-  books: Book[] = [
-    {
-      isbn: 1,
-      title: 'Harry Porter - Book 1',
-      author: '	J. K. Rowling',
-    },
-    {
-      isbn: 2,
-      title: 'Harry Porter - Book 2',
-      author: '	J. K. Rowling',
-    },
-    {
-      isbn: 3,
-      title: 'The Hobbits',
-      author: 'J. R. R. Tolkien',
-    },
-  ];
-
   baseUrl = environment.apiUrl;
   apiUrl = this.baseUrl + 'api/books';
 
   constructor(private http: HttpClient) {}
 
   getBooks(): Observable<Book[]> {
-    return of(this.books);
-    //return this.http.get<Book[]>(this.apiUrl);
+    return this.http.get<Book[]>(this.apiUrl);
   }
 }
