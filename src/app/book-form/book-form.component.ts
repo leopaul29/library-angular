9f82bf82f4bd111c981e32e4027aa0943f5275a5
Leo-paul <leopaulmartin@hotmail.fr>
2021-11-19 23:56:32 +0100

    setup new book form

--- src/app/book-form/book-form.component.ts
+++ src/app/book-form/book-form.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Book } from '../book';
 
 @Component({
   selector: 'app-book-form',
-  templateUrl: './book-form.component.html',
+  template: `
+    <form [formGroup]="bookForm" (ngSubmit)="onSubmit(bookForm.value)">
+      <input type="text" placeholder="Add book" formControlName="title" />
+      <input type="text" placeholder="Add book" formControlName="author" />
+      <button type="submit">Submit</button>
+    </form>
+  `,
   styleUrls: ['./book-form.component.css']
 })
 export class BookFormComponent implements OnInit {
+  bookForm;
+  @Output() addBook = new EventEmitter<any>();
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {
+    this.bookForm = this.formBuilder.group({
+      title: '',
+      author: '',
+    });
+  }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onSubmit(newBook: Book) {
+    this.addBook.emit(newBook);
+    this.bookForm.reset();
   }
 
 }
